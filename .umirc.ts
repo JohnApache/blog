9b0e12b384c8741c5779ac0f3398e463c1f736d4
cjw <1468733552@qq.com>
2020-05-26 13:57:30 +0800

    feat: init blog
    init blog project with dumi

---
+++ .umirc.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'dumi';
+
+export default {
+  ssr: {
+    devServerRender: true,
+    mode: 'stream', // 流式渲染
+  },
+  // exportStatic: {}, // 预渲染
+  ...defineConfig({
+    mode: 'site',
+    title: 'JohnApache',
+    description: 'JohnApache',
+    // logo: '',
+    locales: [], // 不需要多语言
+    // menus: {}, // 约定式 侧边导航
+    navs: [
+      null, // null 值代表保留约定式生成的导航，只做增量配置
+      {
+        title: 'GitHub',
+        path: 'https://github.com/JohnApache',
+      },
+    ],
+    // more config: https://d.umijs.org/config
+  }),
+};
