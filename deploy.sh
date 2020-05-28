662db3873a32bab9edf15e4cb471df87d757f3f7
cjw <1468733552@qq.com>
2020-05-28 11:34:32 +0800

    refactor: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
-
+#!/bin/bash
 cd $SITE_PATH
 echo $PWD
-# git reset --hard origin/master
-# git clean -f
-# git checkout master
-# git pull
-yarn
+git reset --hard origin/master
+git clean -f
+git checkout master
+git pull
+# yarn
 yarn build
