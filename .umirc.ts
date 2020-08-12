f59c7f2b96f93d50e27fbc8732b2cf0062c6e164
cjw <1468733552@qq.com>
2020-08-12 10:40:39 +0800

    build: change umi build config, add split chunk by router
    change umi build config, add split chunk by router

--- .umirc.ts
+++ .umirc.ts
@@ -5,11 +5,25 @@ export default {
   //   devServerRender: true,
   //   mode: 'stream', // 流式渲染
   // },
-  exportStatic: {}, // 预渲染
+  //  exportStatic: {}, // 预渲染
   hash: true,
+  metas: [
+    {
+      name: 'keywords',
+      content:
+        'blog, dumi, 前端博客, 面试题, 算法, 运维, 算法, 数据结构, 前端全栈, 技术文档',
+    },
+    {
+      name: 'description',
+      content: '关注前端技术学习交流，记录与分享自己前端学习历程',
+    },
+  ],
   ...defineConfig({
     mode: 'site',
-    title: 'JohnApache',
+    title: 'JohnApache 的前端日志',
+    dynamicImport: {
+      // loading: ''
+    }, // 启用按需加载
     // description: 'JohnApache',
     // logo: '',
     locales: [], // 不需要多语言
